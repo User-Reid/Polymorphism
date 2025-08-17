3f4cf2a75712481c19b1cdd861acb8717bc4c08b
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-17 17:58:42 -0500

    understanding inheritance and how it can be used. :)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿Console.ReadKey();
+﻿var pizza = new Pizza();
+pizza.AddIngredient(new Cheddar());
+pizza.AddIngredient(new Tomato());
+pizza.AddIngredient(new Mozarella());
+System.Console.WriteLine(pizza.Describe());
+
+Console.ReadKey();
 
 public class Pizza
 {
@@ -6,7 +12,6 @@ public class Pizza
 
   public void AddIngredient(Ingredient addedIngredient) => _ingredients.Add(addedIngredient);
   public string Describe() => $"This is a pizza with {string.Join(", ", _ingredients)}";
-
 }
 
 public class Ingredient
@@ -14,19 +19,19 @@ public class Ingredient
   
 }
 
-public class Cheddar
+public class Cheddar : Ingredient
 {
   public string Name => "Cheddar cheese";
   public int AgedForMonths { get; }
 }
 
-public class Tomato
+public class Tomato : Ingredient
 {
   public string Name => "Tomato sauce";
   public int TomatosIn100Grams { get; }
 }
 
-public class Mozarella
+public class Mozarella : Ingredient
 {
   public string Name => "Mozerella";
   public bool IsLight { get; }
