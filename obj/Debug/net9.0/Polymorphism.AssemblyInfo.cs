96b385d8911cd7beb99e8b3fc05ad86a29c389ab
Reid Herrera <reidherrerabusiness@gmail.com>
2025-08-19 17:24:50 -0500

    better understanding conversion though explicit and implicit conversion.

--- obj/Debug/net9.0/Polymorphism.AssemblyInfo.cs
+++ obj/Debug/net9.0/Polymorphism.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b3d59cbaa11c03fbd8ada525e8e6159055aabc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab4365d0595e255b5d5ad71e1b4ad9881f3792b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Polymorphism")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
